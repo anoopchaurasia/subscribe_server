f0e7d8b7a106577b88e7c999b92348aadadb3b45
jeet <jeet@vumonic.com>
2019-02-27 14:27:56 +0530

    changed

--- controller/email_controller.js
+++ controller/email_controller.js
@@ -752,20 +752,20 @@ let checkEmail = async (emailObj, mail, user_id) => {
         // console.log($(this))
         // console.log(fa);
         if (fa.toLowerCase().indexOf("unsubscribe") != -1 ||
-            ((fa.toLowerCase().indexOf("here") != -1 || fa.toLowerCase().indexOf("click here") != -1) && $(this).parent.text().toLowerCase().indexOf("unsubscribe") != -1) ||
-            fa.toLowerCase().indexOf("preferences") != -1 ||
-            (fa.toLowerCase().indexOf("click here") != -1 && $(this).parent.text().toLowerCase().indexOf("mailing list") != -1) ||
-            fa.toLowerCase().indexOf("subscription") != -1 ||
-            fa.toLowerCase().indexOf("visit this link") != -1 ||
-            fa.toLowerCase().indexOf("do not wish to receive our mails") != -1 ||
-            fa.toLowerCase().indexOf("not receiving our emails") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("not receiving our emails") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("stop receiving emails") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("unsubscribe") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("subscription") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("preferences") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("mailing list") != -1 ||
-            $(this).parent().text().toLowerCase().indexOf("Don't want this") != -1) {
+        fa.toLowerCase().indexOf("preferences") != -1 ||
+        fa.toLowerCase().indexOf("subscription") != -1 ||
+        fa.toLowerCase().indexOf("visit this link") != -1 ||
+        fa.toLowerCase().indexOf("do not wish to receive our mails") != -1 ||
+        fa.toLowerCase().indexOf("not receiving our emails") != -1 ||
+        $(this).parent().text().toLowerCase().indexOf("not receiving our emails") != -1 ||
+        $(this).parent().text().toLowerCase().indexOf("stop receiving emails") != -1 ||
+        $(this).parent().text().toLowerCase().indexOf("unsubscribe") != -1 ||
+        $(this).parent().text().toLowerCase().indexOf("subscription") != -1 ||
+        $(this).parent().text().toLowerCase().indexOf("preferences") != -1 ||
+        $(this).parent().text().toLowerCase().indexOf("mailing list") != -1 ||
+        (fa.toLowerCase().indexOf("click here") != -1 && $(this).parent.text().toLowerCase().indexOf("mailing list") != -1) ||
+        ((fa.toLowerCase().indexOf("here") != -1 || fa.toLowerCase().indexOf("click here") != -1) && $(this).parent.text().toLowerCase().indexOf("unsubscribe") != -1) ||
+        $(this).parent().text().toLowerCase().indexOf("Don't want this") != -1) {
             url = $(this).attr().href;
             console.log(url)
         }
