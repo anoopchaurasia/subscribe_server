dbfce02ead880ed84f3e35c990c229fbc07babb4
jeet <jeet@vumonic.com>
2019-03-01 15:37:29 +0530

    chnaged date

--- controller/email_controller.js
+++ controller/email_controller.js
@@ -700,7 +700,7 @@ async function getRecentEmail(user_id, auth, nextPageToken) {
 }
 
 async function getAllMailBasedOnSubject(user_id, auth, nextPageToken = null) {
-    let responseList = await gmail.users.messages.list({ auth: auth, userId: 'me', includeSpamTrash: true, maxResults: 100, 'pageToken': nextPageToken, q: 'from:notify@* AND after:2018/02/01 ' });
+    let responseList = await gmail.users.messages.list({ auth: auth, userId: 'me', includeSpamTrash: true, maxResults: 100, 'pageToken': nextPageToken, q: 'from:notify@* AND after:2018/01/01 ' });
     if (responseList) {
         console.log(responseList['data']['messages'].length)
         responseList['data']['messages'].forEach(async element => {
