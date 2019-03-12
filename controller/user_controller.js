4259ef1b84737653b75b2eb41e8ec0233278c25e
Jitendra <jeet@vumonic.com>
2019-03-12 18:03:10 +0530

    device info and profile changed

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -4,6 +4,7 @@ let token_model = require('../models/token');
 let auth_token = require('../models/authToken');
 let email = require('../models/email');
 let userDetails = require('../models/userDetail');
+let deviceInfo = require('../models/deviceInfo');
 let router = express.Router();
 
 router.post('/savefcmToken', async (req, res) => {
@@ -32,12 +33,14 @@ router.post('/saveDeviceInfo', async (req, res) => {
     try {
 
         let auth_id = req.body.authID;
+        console.log(req.body.data)
         let doc = await token_model.findOne({ "token": auth_id }).catch(err => {
             console.log(err);
         });
         if (doc) {
-            let tokenInfo = { "user_id": doc.user_id, "fcm_token": token };
-            let tokenDoc = await fcmToken.findOneAndUpdate({ "user_id": doc.user_id }, tokenInfo, { upsert: true }).catch(err => {
+            let deviceData = req.body.data;
+            deviceData['user_id']=doc.user_id;
+            let tokenDoc = await deviceInfo.findOneAndUpdate({ "user_id": doc.user_id }, deviceData, { upsert: true }).catch(err => {
                 console.log(err);
             });
             if (tokenDoc) {
