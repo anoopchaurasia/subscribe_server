89d202863fe7ff2d34e141369ef0c81571995750
Jitendra <jeet@vumonic.com>
2019-03-29 18:30:24 +0530

    pubsub chnaged

--- controller/pubsub_controller.js
+++ controller/pubsub_controller.js
@@ -27,7 +27,7 @@ router.post('/getemail', async (req, response) => {
             
             var options = {
                 userId: 'me',
-                'startHistoryId': historyID-5,
+                'startHistoryId': historyID-10,
                 auth: oauth2Client
             };
             // console.log(options)
