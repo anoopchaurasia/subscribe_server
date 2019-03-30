283f6dc230dee405159ecb5f913e1aa859b5ca53
Jitendra <jeet@vumonic.com>
2019-03-30 11:28:24 +0530

    changed

--- controller/pubsub_controller.js
+++ controller/pubsub_controller.js
@@ -563,7 +563,7 @@ async function MoveMailFromInBOX(user_id, auth, mailList, label) {
 }
 
 
-async function deleteEmailsAndMoveToTrash(user_id, auth, emailInfo) {
+async function deleteEmailsAndMoveToTrash(auth, emailInfo) {
     const gmail = google.gmail({ version: 'v1', auth });
     if (emailInfo.email_id) {
         let modifying = await gmail.users.messages.modify({
