b65e3cbb0cbf13e9c914e6e98f8763947118aa7e
Jitendra <jeet@vumonic.com>
2019-04-16 16:25:04 +0530

    mail scrap api added

--- controller/email_controller.js
+++ controller/email_controller.js
@@ -14,6 +14,7 @@ const DeleteEmail = require("../helper/deleteEmail").DeleteEmail;
 const TrashEmail = require("../helper/trashEmail").TrashEmail;
 
 const APPROX_TWO_MONTH_IN_MS = 2*30*24*60*60*1000;
+const senderList = ["@accounts.google.com"]
 /*
 This api for deleting mail from Inbox or Trash folder.
 */
@@ -202,7 +203,6 @@ This for function for scrapping Inbox for particular user.
 This will Get List of email in Batch of 100 for given Time period and will parsed mail.
 */
 async function getRecentEmail(user_id, auth, nextPageToken) {
-    let senderList = ["@accounts.google.com"]
     let date = new Date(Date.now()-APPROX_TWO_MONTH_IN_MS);
     let formatted_date = `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`; // "2019/2/1";
     let responseList = await gmail.users.messages.list({ auth: auth, userId: 'me', /*includeSpamTrash: true,*/ maxResults: 100, 'pageToken': nextPageToken, q: `from:* AND after:${formatted_date}` });
