68bf9926925eb8bf9c514b41fbeca3ad8157708b
Jitendra <jeet@vumonic.com>
2019-04-22 15:12:52 +0530

    setry added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var Raven = require('raven');
+Raven.config('https://edb20d0741384f7e8ef743a5a22659d5@sentry.expensebit.com/13').install();
 let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_SERVER);
 mongoose.connection.once('connected', function () {
@@ -16,6 +17,7 @@ app.use('/api/v1/mail', require('./routes/router'));
 app.get('/api/v1/setToken', function (req, res) {
     console.log(req)
 })
+
 
 app.get('/', function (req, res) {
     res.send("welcome to express");
