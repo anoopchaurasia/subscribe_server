12ca401cc56c742baf061b67ade28a3b317e9530
Jitendra <jeet@vumonic.com>
2019-05-13 14:26:09 +0530

    cursor for watcher

--- helper/watcher.js
+++ helper/watcher.js
@@ -7,20 +7,24 @@ if(process.env.NODE_APP_INSTANCE ==0) {
     const gmail = google.gmail('v1');
     const schedule = require('node-schedule');
     
-    
+    Array.prototype.asynForEach = async function (cb) {
+        for (let i = 0, len = this.length; i < len; i++) {
+            await cb(this[i]);
+        }
+    }
     /*
         This is schedular Function. This will be called Every Day for all user for Gmail Watch Api Request.
     */
-    schedule.scheduleJob('0 0 * * *',async () => { 
+    schedule.scheduleJob('23 * * * *',async () => { 
         console.log("scheduler called for watch api...");
-        const users = await UserModel.find().catch(e => console.error(e.message, e.stack));
-        users.forEach(async user => {
+        const cursor = UserModel.find().cursor();
+        cursor.eachAsync(async user => {
             const authToken = await TokenHandler.getAccessToken(user._id).catch(e => console.error(e.message, e.stack));
             let oauth2Client = await TokenHandler.createAuthCleint(authToken);
-            await watchapi(oauth2Client);
-        });
+            await watchapi(oauth2Client)
+        }).
+        then(() => console.log('done!'))
     });
-    
     
     /*
         This function for calling Watch Api for User.
