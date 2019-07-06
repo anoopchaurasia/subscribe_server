7072c0a33470c60635a96e6b32c7965ff8bc74ca
Jitendra <jeet@vumonic.com>
2019-07-06 12:43:34 +0530

    response provider

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connection.once('connected', function () {
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api/v1/mail', require('./routes/router'));
+app.use('/api/v2/mail', require('./routes/router'));
 app.get('/api/v2/setToken', function (req, res) {
     console.log(req)
 })
