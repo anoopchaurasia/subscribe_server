2b81776e99a29a67c2dcccbdcbcecd6ee5632df9
Anoop <anoop@vumonic.com>
2019-08-08 15:22:28 +0530

    fix

--- js/com/anoop/email/BaseController.js
+++ js/com/anoop/email/BaseController.js
@@ -104,7 +104,7 @@ fm.Class('BaseController', function (me, EmailDetail, EmailInfo, User,Token, Pro
         return await EmailDetail.updateStatus({ _id: _id }, status);
     };
 
-    Static.updateEmailDetailByFromEmail = function(user_id, from_email, status){
+    Static.updateEmailDetailByFromEmail = async function(user_id, from_email, status){
         return await EmailDetail.updateStatus({ user_id, from_email }, status);
     };
 
