b1eaae827fd417b3503dd1fbd77a658481c66bb2
Anoop <anoop@vumonic.com>
2019-08-19 15:54:48 +0530

    fix links in simpleparser

--- js/com/anoop/imap/Message.js
+++ js/com/anoop/imap/Message.js
@@ -84,7 +84,7 @@ fm.Class("Message", function (me) {
                     stream.once('end', async () => {
                         const raw = Buffer.concat(chunks).toString('utf8');
                         
-                        let parsed = await simpleParser(raw,{skipHtmlToText:true, skipTextToHtml: true, skipTextLinks: true});
+                        let parsed = await simpleParser(raw,{skipHtmlToText:true, skipTextToHtml: true, skipTextLinks: true, skipImageLinks: true});
                         resolve(parsed)
                     });
                 });
