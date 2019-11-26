61cbfc817eb45a523d632cd1e9224d753cba36b7
anoopchaurasia <anoop@vumonic.com>
2019-11-26 11:15:32 +0530

    enhancement

--- helper/process_email_update.js
+++ helper/process_email_update.js
@@ -1,13 +1,13 @@
-fm.Include("com.anoop.imap.Controller");
-fm.Include("com.jeet.memdb.RedisDB");
-let RedisDB = com.jeet.memdb.RedisDB;
-let ImapController = com.anoop.imap.Controller;
-RedisDB.BLPopListner('email_update_for_user', async function(data){
-    try{
-        console.log("user_id", data);
-
-        //ImapController.updateForUser(user_id);
-    }catch(e) {
-        console.error(e);
-    }
+fm.Include("com.anoop.imap.Controller", function(){
+    let RedisDB = com.jeet.memdb.RedisDB;
+    let ImapController = com.anoop.imap.Controller;
+    RedisDB.BLPopListner('email_update_for_user', async function(data){
+        try{
+            console.log("user_id", data);
+    
+            //ImapController.updateForUser(user_id);
+        }catch(e) {
+            console.error(e);
+        }
+    });
 });
