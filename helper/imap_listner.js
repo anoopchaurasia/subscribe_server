c9480d56ff7f8ff2c7f1e27dd36cbdf1caa9fcf7
Anoop <anoop@vumonic.com>
2019-11-27 13:45:23 +0530

    enhancement

--- helper/imap_listner.js
+++ helper/imap_listner.js
@@ -1,6 +1,4 @@
 'use strict'
-console.log("is Master in watcher");
-const UserModel = require('../models/user');
 fm.Include("com.anoop.imap.Controller");
 let ImapController = com.anoop.imap.Controller;
 let RedisDB = com.jeet.memdb.RedisDB;
@@ -12,63 +10,21 @@ Array.prototype.asynForEach = async function (cb) {
 
 let LISTEN_USER_KEY = "listen_for_user";
 
-let cluster = require("cluster");
 
-if(cluster.isMaster) {
-    let instances = process.env.TOTAL_LISTNERS||4;
-    for(let i=0; i< instances; i++) {
-        cluster.fork();
-    }
-
-    setTimeout(x=>{
-        onNewUser();
-        runJob();
-    }, 10*1000);
-
-    async function runJob(offset=0 ){
-        console.log("scheduler called for scrapping mail for imap...");
-        let counter = offset;
-        const cursor = await UserModel.find({ "email_client": "imap" }, {_id: 1}).skip(offset).cursor();
-        cursor.eachAsync(async user => {
-            RedisDB.lPush(LISTEN_USER_KEY, user._id.toHexString());
-            counter++;
-        }).catch(async e => {
-            console.error("watch error", counter, e);
-            if (e.codeName == "CursorNotFound") {
-                runJob(counter);
-            }
-        })
-        .then(() => {
-            console.log('done!')
-        })
-    };
-
-    function onNewUser(){
-        ImapController.onNewUser(async x=>{
-            let user = await ImapController.getUserById(x);
-            console.log("new user added", user);
-            if(user.listener_active && user.inactive_at==null) {
-                return false;
-            }
-            RedisDB.lPush(LISTEN_USER_KEY, user._id.toHexString());
-            console.log("setting for new user", user._id);
-        });
-    }
-}else {
-    RedisDB.BLPopListner(LISTEN_USER_KEY, async function([err, user_id]){
-        let user = await ImapController.getUserById(x);
-        await scrapEmailForIamp(user).catch(err => {
-            console.error(err.message, "user -> ", user.email);
-        });
+RedisDB.BLPopListner(LISTEN_USER_KEY, async function([err, user_id]){
+    let user = await ImapController.getUserById(x);
+    await scrapEmailForIamp(user).catch(err => {
+        console.error(err.message, "user -> ", user.email);
     });
-    async function scrapEmailForIamp(user){
-        console.log("here ->",user.email, cluster.worker.id);
-        await ImapController.updateUserById({_id: user._id}, {listener_active: true});
-        await ImapController.listenForUser(user, "start", function(x, y){
-            console.log(x, y, "new email update");
-            RedisDB.lPush("email_update_for_user", user._id.toHexString() );
-        });
-    };
-}
+});
+async function scrapEmailForIamp(user){
+    console.log("here ->",user.email, cluster.worker.id);
+    await ImapController.updateUserById({_id: user._id}, {listener_active: true});
+    await ImapController.listenForUser(user, "start", function(x, y){
+        console.log(x, y, "new email update");
+        RedisDB.lPush("email_update_for_user", user._id.toHexString() );
+    });
+};
+
 
 
---
+++ imap_listener_master.js
@@ -0,0 +1,71 @@
+'use strict'
+require("dotenv").config({
+  "path": ".listner_env"
+});
+require("dotenv").config();
+require("jsfm");
+fm.basedir = process.cwd() + "/js";
+global.basedir = process.cwd();
+
+var Raven = require('raven');
+Raven.config('https://edb20d0741384f7e8ef743a5a22659d5@sentry.expensebit.com/13').install();
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_SERVER, {
+  useNewUrlParser: true
+});
+mongoose.connection.once('connected', function () {
+  console.log("Connected to database")
+  setTimeout(x => {
+    onNewUser();
+    runJob();
+  }, 10 * 1000);
+});
+
+
+'use strict'
+const UserModel = require('./models/user');
+fm.Include("com.anoop.imap.Controller");
+let ImapController = com.anoop.imap.Controller;
+let RedisDB = com.jeet.memdb.RedisDB;
+Array.prototype.asynForEach = async function (cb) {
+  for (let i = 0, len = this.length; i < len; i++) {
+    await cb(this[i]);
+  }
+}
+
+let LISTEN_USER_KEY = "listen_for_user";
+
+async function runJob(offset = 0) {
+  RedisDB.delKEY(LISTEN_USER_KEY);
+  console.log("scheduler called for scrapping mail for imap...");
+  let counter = offset;
+  const cursor = await UserModel.find({
+    "email_client": "imap"
+  }, {
+    _id: 1
+  }).skip(offset).cursor();
+  cursor.eachAsync(async user => {
+      RedisDB.lPush(LISTEN_USER_KEY, user._id.toHexString());
+      counter++;
+    }).catch(async e => {
+      console.error("watch error", counter, e);
+      if (e.codeName == "CursorNotFound") {
+        runJob(counter);
+      }
+    })
+    .then(() => {
+      console.log('done!')
+    })
+};
+
+function onNewUser() {
+  ImapController.onNewUser(async x => {
+    let user = await ImapController.getUserById(x);
+    console.log("new user added", user);
+    if (user.listener_active && user.inactive_at == null) {
+      return false;
+    }
+    RedisDB.lPush(LISTEN_USER_KEY, user._id.toHexString());
+    console.log("setting for new user", user._id);
+  });
+}
