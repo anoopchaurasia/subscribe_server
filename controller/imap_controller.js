22054934ea17b035454cefc7518559b118bf5bfb
anoopchaurasia <anoop@vumonic.com>
2019-12-02 13:54:18 +0530

    new changes

--- controller/imap_controller.js
+++ controller/imap_controller.js
@@ -497,7 +497,7 @@ router.post('/readZohoMail', async (req, res) => {
         let doc = await token_model.findOne({ "token": req.body.token}).catch(err => {
             console.error(err.message);
         });
-        console.log("got user tokane", token);
+        console.log("got user tokane", doc);
         Controller.sendToProcessServer(doc.user_id);
         
         res.status(200).json({
