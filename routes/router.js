5d43c944d09e52da6ce2a5276a57abd690525e96
anoopchaurasia <anoop@vumonic.com>
2019-12-28 19:38:58 +0530

    jwt login

--- routes/router.js
+++ routes/router.js
@@ -10,7 +10,7 @@ router.use('/imap', noauth, require('../controller/imap_controller'));
 router.use('/imap', authenticate, require('../controller/imap_controller_auth')); 
 router.use('/imap', authenticate, require('../controller/imap_action_controller')); 
 router.use('/imap', authenticate, require('../controller/imap_quick_clean_controller')); 
-
+let jwt = require("jwt");
 async function noauth(req, res, next){
     BaseController.sendToAppsFlyer("temp@temp.com", req.originalUrl.split("/").join("_"));
     next();
@@ -18,6 +18,9 @@ async function noauth(req, res, next){
 
 async function authenticate(req, res, next){
     let token = req.body.authID || req.body.token;
+    if(!token && jwt_login(req, res)) {
+        return;
+    }
     let user = await BaseController.TokenModel.getUserByToken(token);
     if(!user) {
         BaseController.sendToAppsFlyer("nolog", req.originalUrl.split("/").join("_"))
@@ -27,6 +30,29 @@ async function authenticate(req, res, next){
     BaseController.sendToAppsFlyer(user.email, req.originalUrl.split("/").join("_"));
     req.user = user;
     next();
-};
+}
+
+async function jwt_login (req, res, next) {
+    let token = req.headers["x-auth-token"] || req.headers['authorization'];
+    if (!token) {
+        return false;
+    }
+    if(token.startsWith('Bearer ')){
+        token = token.split(' ')[1];
+    }
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, (err, data) => {
+        if (err) {
+            console.error(err.message,err.stack,'jwtTokenVerify');
+            res.status(401).json({
+                error: true,
+                msg: "unauthorised user"
+            });
+        } else {
+            req.token = data;
+        }
+        next();
+    })
+    return true;
+}
 
 module.exports = router;
