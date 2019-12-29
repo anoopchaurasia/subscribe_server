68b4b7575c356a9dacd2f43bceb4dc88135445ab
anoopchaurasia <anoop@vumonic.com>
2019-12-29 13:34:57 +0530

    fixes

--- helper/user_action.js
+++ helper/user_action.js
@@ -14,17 +14,19 @@ fm.Include("com.anoop.imap.Controller", function(){
             action.args[0] = await ImapController.UserModel.getRedisUser(action.args[0]);
             await ImapController[action.action](...action.args, function onDIsconect() {
                 console.warn("disconnected crashing");
+                action  = JSON.parse(data[1]);
                 action.error_count = action.error_count || 0
                 action.error_count++;
-                RedisDB.lPush('imap_user_actions', data[1]);
+                RedisDB.lPush('imap_user_actions', JSON.stringify(action));
             });
         }catch(e) {
             console.error(e);
             if(!e.message.match(global.INVALID_LOGIN_REGEX)) {
                 console.warn("user imap_user_actions crashed restarting reason: ", e.message, data[1]);
+                action  = JSON.parse(data[1]);
                 action.error_count = action.error_count || 0
                 action.error_count++;
-                RedisDB.lPush('imap_user_actions', data[1])
+                RedisDB.lPush('imap_user_actions', JSON.stringify(action))
             }
         }
     });
