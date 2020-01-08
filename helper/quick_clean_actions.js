6d74a0432dc7334beac63dfbd2a141756a256865
anoopchaurasia <anoop@vumonic.com>
2020-01-08 11:00:00 +0530

    qc_restriction 4 times

--- helper/quick_clean_actions.js
+++ helper/quick_clean_actions.js
@@ -5,15 +5,21 @@ fm.Include("com.anoop.imap.Controller", function(){
     let ImapController = com.anoop.imap.Controller;
 
     RedisDB.BLPopListner('qc_scan_user_boxes', async function(data){
-        let user = await ImapController.UserModel.getRedisUser(data[1]);
+        let [user_id, error_count=0] = data[1].split("#");
+        let user = await ImapController.UserModel.getRedisUser(user_id);
+        if(error_count>3) {
+            ImapController.scanFinishedQuickClean(user_id);
+            return console.error("not trying as failed 3 times already",  data);   
+        }
         try{
             console.log("qc_scan_user_boxes", data[1]);
             await ImapController.extractAllEmail(user)
         }catch(e) {
             console.error(e);
             if(!e.message.match(global.INVALID_LOGIN_REGEX)) {
+                error_count = error_count*1;
                 console.warn("user qc_scan_user_boxes crashed restarting reason: ", e.message);
-                RedisDB.lPush('qc_scan_user_boxes', data[1])
+                RedisDB.lPush('qc_scan_user_boxes#'+(error_count+1), data[1])
             } else{
                 await ImapController.scanFinishedQuickClean(user._id);
             }
