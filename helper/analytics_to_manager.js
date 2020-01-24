2f8ed208fcfd938f9e06ada17391ab744406b9fa
anoopchaurasia <anoop@vumonic.com>
2020-01-24 17:27:25 +0530

    multi data

--- helper/analytics_to_manager.js
+++ helper/analytics_to_manager.js
@@ -14,13 +14,16 @@ let datatosend = [];
 async function senddata(message){
     datatosend.push(message);
 }
-
+let os = require("os");
 async function send() {
     if(datatosend.length==0) return;
     let msgs = [... datatosend];
     datatosend = [];
+    // msgs.push({data: process.cpuUsage(), type: "cpu_usage", formatter: "cpu_percentage"});
+    // msgs.push({data:  os.uptime(), type: "uptime", formatter: "seconds"});
+    // msgs.push({data:  os.freemem(), type: "free_ram", formatter: "value"});
     let messages = Buffer.from( JSON.stringify(msgs));
-    await client.send(messages, 41234, process.env.MANAGER_HOST || "10.1.5.90" ||"localhost", (err) => {
+    await client.send({data: messages, type:"multi"}, 41234, process.env.MANAGER_HOST || "10.1.5.90" ||"localhost", (err) => {
         console.error(err);
     });
 }
